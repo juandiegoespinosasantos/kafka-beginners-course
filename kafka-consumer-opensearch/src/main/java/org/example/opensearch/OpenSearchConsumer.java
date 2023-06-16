9990c517cc43ca64c83923296ca20864b391eb79
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2023-06-16 16:37:51 -0500

    kafka-consumer-opensearch module

---
+++ kafka-consumer-opensearch/src/main/java/org/example/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,13 @@
+package org.example.opensearch;
+
+/**
+ * @author juandiegoespinosasantos@gmail.com
+ * @version Jun 16, 2023
+ * @since 11
+ */
+public class OpenSearchConsumer {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
--- kafka-producer-wikimedia/src/main/java/org/example/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/example/wikimedia/WikimediaChangesProducer.java
@@ -41,6 +41,10 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1_024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         return properties;
     }
 
