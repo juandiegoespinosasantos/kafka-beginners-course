8cacadb842c9070fdaf945a51bf7c1e918e2cb8a
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2023-06-22 20:56:18 -0500

    Code improvement

--- kafka-consumer-opensearch/src/main/java/org/example/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/org/example/opensearch/OpenSearchConsumer.java
@@ -11,9 +11,11 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
 import org.opensearch.action.index.IndexRequest;
-import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.IndicesClient;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
@@ -43,8 +45,23 @@ public class OpenSearchConsumer {
     public static void main(String[] args) throws IOException {
         // Create Kafka client
         // Create OpenSearch client
-        try (RestHighLevelClient openSearchClient = createOpenSearchClient();
-             KafkaConsumer<String, String> consumer = createKafkaConsumer()) {
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+        try (openSearchClient; consumer) {
+            final Thread thread = Thread.currentThread();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                LOG.info("Shutdown detected...");
+
+                consumer.wakeup();
+
+                try {
+                    thread.join();
+                } catch (InterruptedException ex) {
+                    LOG.error(ex.getMessage(), ex);
+                }
+            }));
+
             String index = "wikimedia";
             IndicesClient indices = openSearchClient.indices();
 
@@ -66,27 +83,48 @@ public class OpenSearchConsumer {
 
                 LOG.info("Received {} records", records.count());
 
+                BulkRequest bulkRequest = new BulkRequest();
+
                 for (ConsumerRecord<String, String> record : records) {
 //                    String id = record.topic() + "_" + record.partition() + "_" + record.offset();
                     String id = extractId(record.value());
 
                     try {
-                        IndexRequest indexRequest = new IndexRequest("wikimedia")
-                                .id(id)
-                                .source(record.value(), XContentType.JSON);
-                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        IndexRequest indexRequest = new IndexRequest("wikimedia").id(id).source(record.value(), XContentType.JSON);
+                        bulkRequest.add(indexRequest);
 
-                        LOG.info(response.getId());
+//                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+//                        LOG.info(response.getId());
                     } catch (Exception ex) {
                         LOG.warn(ex.getMessage());
                     }
                 }
+
+                if (bulkRequest.numberOfActions() > 0) {
+                    BulkResponse bulkResponse = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    LOG.info("Inserted " + bulkResponse.getItems().length + " records");
+
+                    try {
+                        Thread.sleep(1_000L);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    // Commit offsets after the batch is consumed
+                    consumer.commitAsync();
+                    LOG.info("Offsets have been committed");
+                }
             }
+        } catch (WakeupException ex) {
+            LOG.info("Consumer shutting down...");
+        } catch (Exception ex) {
+            LOG.error(ex.getMessage(), ex);
+        } finally {
+            consumer.close();
+            openSearchClient.close();
+
+            LOG.info("Shut down!");
         }
-
-        // Main code logic
-
-        // Close things
     }
 
     private static KafkaConsumer<String, String> createKafkaConsumer() {
@@ -97,6 +135,7 @@ public class OpenSearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         return new KafkaConsumer<>(properties);
     }
@@ -117,20 +156,13 @@ public class OpenSearchConsumer {
             CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
             credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
 
-            restClientBuilder = RestClient.builder(new HttpHost(host, port, uri.getScheme()))
-                    .setHttpClientConfigCallback(asyncClientBuilder -> asyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy()));
+            restClientBuilder = RestClient.builder(new HttpHost(host, port, uri.getScheme())).setHttpClientConfigCallback(asyncClientBuilder -> asyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider).setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy()));
         }
 
         return new RestHighLevelClient(restClientBuilder);
     }
 
     private static String extractId(final String jsonValue) {
-        return JsonParser.parseString(jsonValue)
-                .getAsJsonObject()
-                .get("meta")
-                .getAsJsonObject()
-                .get("id")
-                .getAsString();
+        return JsonParser.parseString(jsonValue).getAsJsonObject().get("meta").getAsJsonObject().get("id").getAsString();
     }
 }
